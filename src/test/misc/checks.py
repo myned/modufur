49fcdad56efe9f8a0f4d5e656ef8e5f49021e536
Myned <alphasea14@gmail.com>
2017-09-24 11:05:28 -0400

    Initial commit

---
+++ src/test/misc/checks.py
@@ -0,0 +1,45 @@
+import asyncio
+import discord
+import json
+import traceback
+from discord.ext import commands
+from discord.ext.commands import errors
+
+with open('config.json') as infile:
+    config = json.load(infile)
+
+owner_id = config['owner_id']
+
+def is_owner():
+    async def predicate(ctx):
+        return ctx.message.author.id == owner_id
+    return commands.check(predicate)
+def is_admin():
+    def predicate(ctx):
+        return ctx.message.author.guild_permissions.administrator
+    return commands.check(predicate)
+def is_mod():
+    def predicate(ctx):
+        return ctx.message.author.guild_permissions.ban_members
+    return commands.check(predicate)
+
+def owner(ctx):
+    return ctx.message.author.id == owner_id
+def admin(ctx):
+    return ctx.message.author.guild_permissions.administrator
+def mod(ctx):
+    return ctx.message.author.guild_permissions.ban_members
+
+def is_nsfw():
+    def predicate(ctx):
+        if isinstance(ctx.message.channel, discord.TextChannel):
+            return ctx.message.channel.is_nsfw()
+        return True
+    return commands.check(predicate)
+
+def del_ctx():
+    async def predicate(ctx):
+        if isinstance(ctx.message.channel, discord.TextChannel):
+            await ctx.message.delete()
+        return True
+    return commands.check(predicate)
---
+++ src/test/utils/formatter.py
@@ -0,0 +1,31 @@
+def tostring(i):
+    o = ' '
+    if i:
+        for v in i:
+            o += v + ' '
+        o = o[1:-1]
+    return o
+
+def tostring_commas(i):
+    if i:
+        o = ','
+        for v in i:
+            o += v + ','
+        return o[:-1]
+    return ''
+
+def dict_tostring(i):
+    o = ''
+    if i:
+        for k, v in i.items():
+            o += '**' + k + ':** `' + tostring(v) + '`\n'
+    return o
+
+def dictelem_tostring(i):
+    o = ''
+    if i:
+        for dic, elem in i.items():
+            o += '**__' + dic + '__**\n'
+            for k, v in elem.items():
+                o += '***' + k + ':*** `' + tostring(v) + '`\n'
+    return o
