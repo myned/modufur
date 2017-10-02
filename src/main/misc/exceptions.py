08d7f160f8c62f3782c3de1d51ff6c66f4ffb77e
Myned <alphasea14@gmail.com>
2017-10-02 19:38:44 -0400

    Compacted class formatter for visibility

--- src/main/misc/exceptions.py
+++ src/main/misc/exceptions.py
@@ -1,28 +1,18 @@
 base = '‼️ **An internal error has occurred.** Please notify my master! 🐺'
 
-class PostError(Exception):
-    pass
-class ImageError(Exception):
-    pass
-class MatchError(Exception):
-    pass
-class TagBlacklisted(Exception):
-    pass
-class BoundsError(Exception):
-    pass
-class TagBoundsError(Exception):
-    pass
-class TagExists(Exception):
-    pass
-class TagError(Exception):
-    pass
-class FlagError(Exception):
-    pass
-class BlacklistError(Exception):
-    pass
-class NotFound(Exception):
-    pass
-class Timeout(Exception):
-    pass
-class Continue(Exception):
-    pass
+class PostError(Exception): pass
+class ImageError(Exception): pass
+class MatchError(Exception): pass
+class TagBlacklisted(Exception): pass
+class BoundsError(Exception): pass
+class TagBoundsError(Exception): pass
+class TagExists(Exception): pass
+class TagError(Exception): pass
+class FlagError(Exception): pass
+class BlacklistError(Exception): pass
+class NotFound(Exception): pass
+class Timeout(Exception): pass
+class InvalidVideoFile(Exception): pass
+class MissingAttachment(Exception): pass
+class TooManyAttachments(Exception): pass
+class Continue(Exception): pass
