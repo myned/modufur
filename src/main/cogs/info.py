6f38e0bc8e8ebdd138697da20a6f7f7197a40f8a
Myned <alphasea14@gmail.com>
2017-10-17 19:04:45 -0400

    4 space > 2 space

--- src/main/cogs/info.py
+++ src/main/cogs/info.py
@@ -10,40 +10,40 @@ from utils import utils as u
 
 class Info:
 
-    def __init__(self, bot):
-        self.bot = bot
+  def __init__(self, bot):
+    self.bot = bot
 
-    # @commands.command(name='helptest', aliases=['h'], hidden=True)
-    # async def list_commands(self, ctx):
-    #     embed = d.Embed(title='All possible commands:', color=ctx.me.color)
-    #     embed.set_author(name=ctx.me.display_name, icon_url=ctx.me.avatar_url)
-    #     embed.add_field(
-    #         name='Booru', value='\n{}bl umbrella command for managing blacklists'.format(u.config['prefix']))
-    #
-    #     await ctx.send(embed=embed)
+  # @commands.command(name='helptest', aliases=['h'], hidden=True)
+  # async def list_commands(self, ctx):
+  #     embed = d.Embed(title='All possible commands:', color=ctx.me.color)
+  #     embed.set_author(name=ctx.me.display_name, icon_url=ctx.me.avatar_url)
+  #     embed.add_field(
+  #         name='Booru', value='\n{}bl umbrella command for managing blacklists'.format(u.config['prefix']))
+  #
+  #     await ctx.send(embed=embed)
 
-    @commands.command(hidden=True)
-    async def hi(self, ctx, *args):
-        dest = u.get_args(ctx, args)
+  @commands.command(hidden=True)
+  async def hi(self, ctx, *args):
+    dest = u.get_args(ctx, args)
 
-        hello = 'Hewwo, {}.'.format(ctx.author.mention)
-        if ctx.author.id == checks.owner_id:
-            hello += '.. ***Master.*** uwu'
-        elif ctx.author.guild_permissions.administrator:
-            hello = '{} **Admin** {}'.format(hello[:7], hello[7:])
-        elif ctx.author.guild_permissions.ban_members:
-            hello = '{} **Mod** {}'.format(hello[:7], hello[7:])
-        await dest.send(hello)
+    hello = 'Hewwo, {}.'.format(ctx.author.mention)
+    if ctx.author.id == checks.owner_id:
+      hello += '.. ***Master.*** uwu'
+    elif ctx.author.guild_permissions.administrator:
+      hello = '{} **Admin** {}'.format(hello[:7], hello[7:])
+    elif ctx.author.guild_permissions.ban_members:
+      hello = '{} **Mod** {}'.format(hello[:7], hello[7:])
+    await dest.send(hello)
 
-    @commands.group(name='info', aliases=['i'])
-    async def info(self, ctx):
-        if invoked_subcommand is None:
-            await ctx.send('<embed>BOT INFO</embed>')
+  @commands.group(name='info', aliases=['i'])
+  async def info(self, ctx):
+    if invoked_subcommand is None:
+      await ctx.send('<embed>BOT INFO</embed>')
 
-    @info.command(aliases=['g', 'server', 's'], brief='Provides info about a guild', hidden=True)
-    async def guild(self, ctx):
-        pass
+  @info.command(aliases=['g', 'server', 's'], brief='Provides info about a guild', hidden=True)
+  async def guild(self, ctx):
+    pass
 
-    @info.command(aliases=['u', 'member', 'm'], brief='Provides info about a user', hidden=True)
-    async def user(self, ctx):
-        pass
+  @info.command(aliases=['u', 'member', 'm'], brief='Provides info about a user', hidden=True)
+  async def user(self, ctx):
+    pass
--- src/main/utils/scraper.py
+++ src/main/utils/scraper.py
@@ -8,26 +8,26 @@ from utils import utils as u
 
 
 async def get_post(url):
-    content = await u.fetch('http://iqdb.harry.lu', params={'url': url})
+  content = await u.fetch('http://iqdb.harry.lu', params={'url': url})
 
+  try:
+    value = BeautifulSoup(content, 'html.parser').find_all('a')[1].get('href')
+    if value != '#':
+      return value
+    else:
+      raise IndexError
+  except IndexError:
     try:
-        value = BeautifulSoup(content, 'html.parser').find_all('a')[1].get('href')
-        if value != '#':
-            return value
-        else:
-            raise IndexError
-    except IndexError:
-        try:
-            raise exc.MatchError(re.search('\/([^\/]+)$', url).group(1))
+      raise exc.MatchError(re.search('\/([^\/]+)$', url).group(1))
 
-        except AttributeError:
-            raise exc.MissingArgument
+    except AttributeError:
+      raise exc.MissingArgument
 
 
 async def get_image(url):
-    content = await u.fetch(url)
+  content = await u.fetch(url)
 
-    value = html.fromstring(content).xpath(
-        'string(/html/body/div[@id="content"]/div[@id="post-view"]/div[@class="content"]/div[2]/img/@src)')
+  value = html.fromstring(content).xpath(
+      'string(/html/body/div[@id="content"]/div[@id="post-view"]/div[@class="content"]/div[2]/img/@src)')
 
-    return value
+  return value
