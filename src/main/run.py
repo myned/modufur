22c235386fd62b8e7018dd33b1e2482b6afedcce
Myned <alphasea14@gmail.com>
2017-12-05 01:51:22 -0500

    Changed bot startup channel message to include contextual prefixes

--- src/main/run.py
+++ src/main/run.py
@@ -81,7 +81,7 @@ async def on_ready():
         await bot.change_presence(game=d.Game(name=u.config['playing']))
 
     print('\n> > > > > > > > >\nC O N N E C T E D : {}\n> > > > > > > > >\n'.format(bot.user.name))
-    await bot.get_channel(u.config['info_channel']).send('**Started** \N{BLACK SUN WITH RAYS} .')
+    await bot.get_channel(u.config['info_channel']).send(f'**Started** \N{BLACK SUN WITH RAYS} `{"` or `".join(u.settings["prefixes"][ctx.guild.id] if ctx.guild.id in u.settings["prefixes"] else u.config["prefix"])}`')
     # u.notify('C O N N E C T E D')
 
     if u.temp['startup']:
