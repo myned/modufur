496d5bc00cd1e78157c6b6e8afeee7d644aa8311
Myned <onemyned@gmail.com>
2019-09-29 23:57:00 -0400

    Change HTML sanitization to be more simplistic

--- src/utils/scraper.py
+++ src/utils/scraper.py
@@ -43,9 +43,10 @@ from utils import utils as u
 async def query_kheina(url):
     content = await u.fetch('https://kheina.com', params={'url': url}, text=True)
 
-    content = content.replace('&quot;', 'quot;').replace('&apos;', 'apos;')
+    for e in ('&quot;', '&apos;'):
+        content = content.replace(e, '')
     soup = BeautifulSoup(content, 'html5lib')
-    results = soup.find('data', id='results').string.replace('quot;', '&quot;').replace('apos;', '&apos;')
+    results = soup.find('data', id='results').string
     results = ast.literal_eval(results)
     iqdbdata = soup.find('data', id='iqdbdata').string
     iqdbdata = ast.literal_eval(iqdbdata)
