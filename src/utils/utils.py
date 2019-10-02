65329b81d46595ada92ca8f7c331c5e894cdad72
Myned <onemyned@gmail.com>
2019-10-01 21:58:32 -0400

    Change wording to align with existing terms

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -24,7 +24,7 @@ except FileNotFoundError:
     with open('config.json', 'w') as outfile:
         jsn.dump({'client_id': 0, 'owner_id': 0, 'permissions': 126016,
                   'playing': 'a game', 'prefix': [',', 'm,'], 'selfbot': False, 'token': 'str', 'saucenao_api': 'str'}, outfile, indent=4, sort_keys=True)
-        print('FILE NOT FOUND : config.json created with abstract values. Restart run.py with correct values')
+        print('FILE NOT FOUND : config.json created with default values. Restart run.py with correct values')
 
 
 def setdefault(filename, default=None, json=False):
